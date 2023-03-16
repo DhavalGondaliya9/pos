716c62ecca3cca2604cb3f2075f8a36f4ea71fbe
Dhavalgu <dhavalgondaliya3@gmail.com>
2023-03-16 23:08:03 +0530

    add validation for add button and change input

--- custom.js
+++ custom.js
@@ -2,8 +2,6 @@ function displayProducts() {
     return {
         error: {},
         errors:false, 
-        productData:[],
-        countId : 0,
         title : '',
         invoice : '',
         dateTime : '',
@@ -11,12 +9,6 @@ function displayProducts() {
         quantity : '',
         productName : '',
         amount : '',
-        quantityArray : [],
-        productNameArray : [],
-        amountArray : [],
-        quantityError : [],
-        productNameError : [],
-        amountError : [],
         subtotal: 0,
         tax: '',
         totalTax: 0,
@@ -24,6 +16,13 @@ function displayProducts() {
         cash: '',
         change: '',
         footer: '',
+        productData:[],
+        quantityArray : [],
+        productNameArray : [],
+        amountArray : [],
+        quantityError : [],
+        productNameError : [],
+        amountError : [],
 
         addProduct() {
             this.productValidation();
@@ -34,13 +33,7 @@ function displayProducts() {
             this.quantityArray[length]      = this.quantity;
             this.productNameArray[length]   = this.productName;
             this.amountArray[length]        = this.amount;
-            this.countId++;
-            this.productData.push({
-                id          : this.countId,
-                quantity    : this.quantity,
-                name        : this.productName,
-                amount      : this.amount,
-            });
+
             this.quantity       = '';
             this.productName    = '';
             this.amount         = '';
@@ -49,29 +42,73 @@ function displayProducts() {
         productValidation() {
             this.error  = {};
             this.errors = false;
-            // if (this.quantity == "") {
-            //     this.errors = true;
-            //     this.error.quantity = 'Quantity field required';
-            // }
+            if (this.quantity == "") {
+                this.errors = true;
+                this.error.quantity = 'Quantity field required';
+            } else {
+                
+                if (this.quantity <= 0) {
+                    this.errors = true;
+                    this.error.quantity = 'Quantity cannot be negative or 0';
+                }
+            }
 
-            // if (this.productName == "") {
-            //     this.errors = true;
-            //     this.error.productName = 'Product Name field required';
-            // }
+            if (this.productName == "") {
+                this.errors = true;
+                this.error.productName = 'Product Name field required';
+            }
 
-            // if (this.amount == "") {
-            //     this.errors = true;
-            //     this.error.amount = 'Amount field required';
-            // }
+            if (this.amount == "") {
+                this.errors = true;
+                this.error.amount = 'Amount field required';
+            } else {
+                if (this.amount < 0) {
+                    this.errors = true;
+                    this.error.amount = 'Amount cannot be negative';
+                }
+            }
         },
 
-        removeProduct(product) {
-            this.productData.splice(this.productData.indexOf(product), 1);
+        removeProduct(index) {
+            this.quantityArray.splice(this.quantityArray.indexOf(index), 1);
+            this.productNameArray.splice(this.productNameArray.indexOf(index), 1);
+            this.amountArray.splice(this.amountArray.indexOf(index), 1);
             this.updateTotal();
         },
+        changeCash() {
+            if(this.cash > 0){
+                this.change = parseInt(this.cash - this.grandTotal);
+            }
+        },
         changeValue(event,index) {
-            if (event.target.value == '') {
-                this.quantityError[index] = 'required';
+            if (event.target.name == 'quantity') {
+                this.quantityError[index] = ''; 
+                if (event.target.value == '') {
+                    this.quantityError[index] = 'Quantity field required';
+                } else {
+
+                    if (event.target.value <= 0) {
+                        this.quantityError[index] = 'Quantity cannot be negative or 0';
+                    }
+                }
+            }
+            if (event.target.name == 'productName') {
+                this.productNameError[index] = ''; 
+                if (event.target.value == '') {
+                    this.productNameError[index] = 'Product Name field required';
+                }
+            }
+            if (event.target.name == 'amount') {
+                this.amountError[index] = ''; 
+                if (event.target.value == '') {
+                    this.amountError[index] = 'Amount field required';
+                } else {
+                    if (event.target.value < 0) {
+                        this.amountError[index] = 'Amount cannot be negative';
+                    }
+                }
+
+                this.updateTotal();
             }
         },
 
@@ -80,16 +117,16 @@ function displayProducts() {
             this.totalTax   = 0;
             this.grandTotal = 0;
 
-            this.productData.map((product) => {
-                this.subtotal += parseInt(product.amount);
+            this.amountArray.map((amount) => {
+                this.subtotal += parseInt(amount);
             });
+
             this.totalTax   = (this.subtotal * this.tax) / 100;
             this.grandTotal = this.subtotal + this.totalTax;
             this.totalTax   = this.totalTax.toFixed(2);
             this.subtotal   = this.subtotal.toFixed(2);
             this.grandTotal = this.grandTotal.toFixed(2);
-            console.log(this.quantityArray);
-            
+            this.changeCash();
         },
 
         generatePDF() {
@@ -114,8 +151,44 @@ function displayProducts() {
                 this.errors = true;
                 this.error.address = 'Address field required';
             }
+
+            // this.amountArray.map((amount) => {
+            //     this.subtotal += parseInt(amount);
+            // });
+
+            this.quantityError = [];
+            this.productNameError = [];
+            this.amountError = [];
+            this.amountArray.map(function(element, index) {
+                this.quantityError[index] = ''; 
+                if (this.quantityArray[index] == '') {
+                    this.errors = true;
+                    this.quantityError[index] = 'Quantity field required';
+                } else {
+                    if (this.quantityArray[index] <= 0) {
+                        this.errors = true;
+                        this.quantityError[index] = 'Quantity cannot be negative or 0';
+                    }
+                }
+                this.productNameError[index] = ''; 
+                if (this.productNameArray[index] == '') {
+                    this.errors = true;
+                    this.productNameError[index] = 'Product Name field required';
+                }
+
+                this.amountError[index] = ''; 
+                if (this.amountArray[index] == '') {
+                    this.errors = true;
+                    this.amountError[index] = 'Amount field required';
+                } else {
+                    if (this.amountArray[index] < 0) {
+                        this.errors = true;
+                        this.amountError[index] = 'Amount cannot be negative';
+                    }
+                }
+            });
             
-            if (errors) {
+            if (this.errors) {
                 return false;
             }  
             // var printContents = document.getElementById("my-html").innerHTML;
